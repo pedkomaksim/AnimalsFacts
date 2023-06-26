27386ea68cfc00f05854835413a7f4aa0542171f
Maksym Pedko <maksim.pedko@yalantis.net>
2023-06-26 16:28:08 +0300

    [add] create empty project

---
+++ AnimalFactsAPP/AnimalFactsAPP/AnimalFactsAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimalFactsAPPApp.swift
+//  AnimalFactsAPP
+//
+//  Created by Максим Педько on 26.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimalFactsAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
